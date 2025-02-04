a1651ecffb103d666462f7c83dd1223504c753d2
unknown <delosreyesjohnpaul30@gmail.com>
2025-02-04 19:57:20 +0800

    ₱

--- components/products/ProductColumns.tsx
+++ components/products/ProductColumns.tsx
@@ -28,11 +28,11 @@ export const columns: ColumnDef<ProductType>[] = [
   },
   {
     accessorKey: "price",
-    header: "Price ($)",
+    header: "Price (₱)",
   },
   {
     accessorKey: "expense",
-    header: "Expense ($)",
+    header: "Expense (₱)",
   },
   {
     id: "actions",
